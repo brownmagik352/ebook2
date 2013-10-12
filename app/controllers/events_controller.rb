257376f887d77875a1f2eca8a14c26fc01f7cb0f
brownmagik352 <asuman@Apurvs-MacBook-Pro-2.local>
2013-10-12 15:47:56 -0400

    add views and controller actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,17 +1,29 @@
 class EventsController < ApplicationController
+
+  before_filter :get_event, :only => [:edit, :update, :show, :destroy]
+
   def index
+    @events = Event.all
   end
 
   def new
+    @event = Event.new
   end
 
   def create
+    @event = Event.create(params[:event])
+    @event.save
+
+    redirect_to events_path
   end
 
   def edit
   end
 
   def update
+    @event.update_attributes(params[:event])
+
+    redirect_to events_path
   end
 
   def show
@@ -19,4 +31,9 @@ class EventsController < ApplicationController
 
   def destroy
   end
+
+  def get_event
+    @event = Event.find(params[:id])
+  end
+
 end
